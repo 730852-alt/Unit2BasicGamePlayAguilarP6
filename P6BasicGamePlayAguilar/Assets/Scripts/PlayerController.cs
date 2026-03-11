2a41c3af6a0127e38cc8a9560c5b0979cdc2242a
730852-alt <730852@student.dusd.net>
2026-03-11 13:49:56 -0700

    upodated

--- P6BasicGamePlayAguilar/Assets/Scripts/PlayerController.cs
+++ P6BasicGamePlayAguilar/Assets/Scripts/PlayerController.cs
@@ -29,9 +29,9 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
         
-         if (Input.GetKeyDown(KeyCode.Space));
+         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Instantiate (projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
